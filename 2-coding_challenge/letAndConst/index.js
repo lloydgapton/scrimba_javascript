bca71cfe3fa528b5077f45cb69398da7bf94df26
lloydgapton <lloydgapton123@gmail.com>
2023-11-12 09:28:05 -0800

    update

---
+++ 2-coding_challenge/letAndConst/index.js
@@ -0,0 +1,11 @@
+const player = "Abdulquadri"
+const opponent = "Olanrewaju"
+const game = "amazingFighter"
+let points  = 0
+let hasWon = false
+points += 100
+hasWon = true
+if (hasWon) {
+	console.log(`${player} got ${points} and has won the ${game} game!`)
+else {
+	console.log(`The winner is ${opponent}! and ${player} lost the game!`)
