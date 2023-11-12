7954b00e7143285550e2d68c654b1ffd0ec196df
lloydgapton <lloydgapton123@gmail.com>
2023-11-12 08:47:47 -0800

    added project files

---
+++ 1-coding_challenge/sortingFruits/index.js
@@ -0,0 +1,14 @@
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+
+function sortFruits(){
+	for (let i = 0; i < fruit.length; i++){
+		if (fruit[i] === "🍎"){
+			appleShelf.textContent += "🍎 "
+		}else if (fruit[i] === "🍊){
+			orangeShelf.textContent += "🍊"
+		}
+	}
+}
+sortFruits()
