e52c732c69a096d8dc807c9358398aabc99b0b98
lloydgapton <lloydgapton123@gmail.com>
2023-11-12 23:08:18 -0800

    update

---
+++ 1-coding_challenge/LogicalOperators/index.js
@@ -0,0 +1,6 @@
+let largeCountries = ["Tuvalu", "India", "Usa", "Indonesia", "Monaco"]
+largeCountries.pop()
+largeCountries.push("Pakistan")
+largeCountries.shift()
+largeCountries.unshift("China")
+console.log(largeCountries)
